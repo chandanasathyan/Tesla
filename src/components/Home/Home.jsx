e1b2490fff9eefcda9eb5d3a3da1abd05a90b0d5
chandana <chandanasathyan1997@gmail.com>
2025-01-18 00:19:08 +0530

    imaged added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+
 import banner from "../../assets/tesla-banner.jpg";
 import Header from '../Header/Header';
 import { HomeWrapper, SectionWrapper } from "./style";
+import modely from "../../assets/model-y.jpg"
+import models from "../../assets/model-s3.jpg"
+import modelX from "../../assets/model-x.jpg"
+import model3 from "../../assets/model-3.jpg"
+import { Typography } from "@mui/material";
 
 const Home = () => {
   return (
     <div>
-        <HomeWrapper>
-            <Header />
-            <img src={banner} alt='tela-banner' style={{width: "100%", objectFit: "cover"}} />
-        </HomeWrapper>
+      <HomeWrapper>
+        <Header />
+        <img src={banner} alt='tesla-banner' style={{ width: "100%", objectFit: "cover" }} />
+      </HomeWrapper>
 
-        <SectionWrapper></SectionWrapper>
+      <SectionWrapper>
+        <img src={modely} width="100%" />
+        <Typography>Model 3</Typography>
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <img src={models} width="100%" />
+        <Typography>Model S</Typography>
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <img src={modelX} width="100%" />
+        <Typography>Model X</Typography>
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <img src={model3} width="100%" />
+        <Typography>Model Y</Typography>
+      </SectionWrapper>
     </div>
   )
 }
